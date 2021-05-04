c4d7da3a07b30281f32085617fabfe2a8f8a5b94
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-04 21:46:32 +0430

    quiz1 done

--- quiz1.js
+++ quiz1.js
@@ -1,4 +1,5 @@
-// Write your code in here
+// Select the image element by it's id and save it to a variable called image
+const image = document.getElementById("unicorn");
 
-// . Select the image element by it's id and save it to a variable called image
-// . Select the h1 by it's id and save it to a variable called heading
+// Select the h1 by it's id and save it to a variable called heading
+const heading = document.getElementById("mainheading");
