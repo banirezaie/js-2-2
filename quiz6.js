e33417aab606e0184a127cfd04f970a62f721c86
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-05 00:02:41 +0430

    quiz6 updated

--- quiz6.js
+++ quiz6.js
@@ -6,4 +6,16 @@ const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 the first span should be red, second one orange and so on.
  */
 
+/* ------------------------------ Best Practice ----------------------------- */
+
 document.querySelectorAll("h1 span").forEach(({ style }, index) => (style.color = colors[index]));
+
+/* -------------------------------- Long Way -------------------------------- */
+
+/* 
+const rainbow = document.querySelectorAll("span");
+let count = 0;
+for (const rainbowColor of rainbow) {
+	rainbowColor.style.color = colors[count++];
+}
+ */
