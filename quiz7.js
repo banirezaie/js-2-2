a095be7a16e819c3c15b3e8aa48425cbbd09e6fa
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-05 00:54:02 +0430

    quiz7 done

--- quiz7.js
+++ quiz7.js
@@ -1,8 +1,20 @@
-//in quiz7.html you will find 6 li elements, two of which have the 'highlight' class applied to them.
+/* In quiz7.html you will find 6 li elements, two of which have the 'highlight' class applied to them.
 
-// 1-please use JS and classList property to invert which elements have the highlight class.
-//basically iterate over all the li elements and toggle the class of highlight on each one.
+1- Please use JS and classList property to invert which elements have the highlight class.
+Basically iterate over all the li elements and toggle the class of highlight on each one.
 
-// = your end result will be all blue ish ones will be purple, and all purples will be blue :)
+=> your end result will be all blueish ones will be purple, and all purples will be blue :)
+ */
 
-//you code goes here:
+/* ------------------------------ Best Practice ----------------------------- */
+
+document.querySelectorAll("ul li").forEach(({ classList }) => classList.toggle("highlight"));
+
+/* -------------------------------- Long Way -------------------------------- */
+
+/* 
+const liTags = document.querySelectorAll("ul li");
+for (const li of liTags) {
+	li.classList.toggle("highlight");
+}
+ */
