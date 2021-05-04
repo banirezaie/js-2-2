c6b9654c56d8e18513b474ec19de34a669de7b85
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-04 22:56:25 +0430

    Merge from main

---
+++ event.js
@@ -0,0 +1,32 @@
+// const btn = document.querySelector("#v2")
+// console.log(btn)
+// console.dir(btn)
+
+// btn.onclick = function () {
+//   console.log("you clicked me")
+// }
+
+//and another way of writing codes here:
+
+// function scream() {
+//   console.log('AAAAAAAAAHHHHHH')
+//   console.log("STOP TOUCHING ME")
+// }
+
+// btn.onmouseenter = scream;
+
+//another way:
+// document.querySelector('h1').onclick = ()=> alert('enough here, go to next slide :)')
+
+//addEventListener
+
+// const btn3 = document.querySelector('#v3');
+// btn3.addEventListener('dblclick', function () {
+//   alert('Clicked')
+// })
+
+// btn3.addEventListener('mouseup', function () {
+//   alert('Clicked for mouseup event')
+// })
+
+// btn3.addEventListener('click', scream, {once : true})
