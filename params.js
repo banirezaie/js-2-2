715ca19e4e6440a2eced527b82d662dfb7e14862
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-04 20:11:24 +0430

    params learned

--- params.js
+++ params.js
@@ -1,45 +1,64 @@
-// function rollDie(nmSides) {
-//   return Math.floor(Math.random() * nmSides) + 1
-// }
+//TODO Uncomment each section and give reasons for the output
 
-// console.log(rollDie(6))
-console.log(rollDie())
+/* 
+function rollDie(numSides) {
+  return Math.floor(Math.random() * numSides) + 1
+}
 
-// function rollDie(numSides) {
-//   if (numSides === undefined) {
-//     numSides = 6
-//   }
+console.log(rollDie(6)) 
+console.log(rollDie()) //=> NaN
+ */
 
-//   return Math.floor(Math.random() * numSides) + 1
-// }
+/* 
+function rollDie(numSides) {
+	if (numSides === undefined) {
+		numSides = 6;
+	}
 
+	return Math.floor(Math.random() * numSides) + 1;
+}
+console.log(rollDie()); //=> A random number between 1, 6
+ */
+
+/* 
 function rollDie(numSides = 6) {
   return Math.floor(Math.random() * numSides) + 1
 }
 
-//ORDER MATTERS HERE
+console.log(rollDie()); //=> A random number between 1, 6
+ */
 
-// function greet(msg, person) {
-//   console.log(`${msg} , ${person}`)
-// }
+//! ORDER MATTERS HERE
 
-// greet("hello", "Mozafar")
+/* 
+function greet(msg, person) {
+  console.log(`${msg} , ${person}`)
+}
 
-// function greet(msg = "Hey there", person) {
-//   console.log(`${msg} , ${person}`)
-// }
+greet("hello", "Mozafar") //=> hello , Mozafar
+ */
 
-// greet("Mozafar")
+/* 
+function greet(msg = "Hey there", person) {
+	console.log(`${msg} , ${person}`);
+}
 
-// function greet(person, msg = "Hey there") {
-//   console.log(`${msg} , ${person}`)
-// }
+greet("Mozafar"); //=> Mozafar , undefined
+ */
 
-// greet("Mozafar")
+/* 
+function greet(person, msg = "Hey there") {
+  console.log(`${msg} , ${person}`)
+}
 
-// function greet(person, msg = "Hey there", punc = "!") {
-//   console.log(`${msg} , ${person}${punc}`)
-// }
+greet("Mozafar") //=> Hey there , Mozafar
+ */
 
-// greet("Mozafar")
-// greet("Mozafar", "hellooooo", "!!!!!")
+/* 
+function greet(person, msg = "Hey there", punc = "!") {
+	console.log(`${msg} , ${person}${punc}`);
+}
+
+greet("Mozafar"); //=> Hey there , Mozafar!
+greet("Mozafar", "hellooooo", "!!!!!"); //=> hellooooo , Mozafar!!!!!
+ */
