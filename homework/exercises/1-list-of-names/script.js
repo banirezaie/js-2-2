8e975aafe985bc920413574f5de39bbdd93684a5
BANI GIT <bani.rezaie@gmail.com>
2021-05-05 14:17:42 +0100

    final changes on Tuesday

---
+++ homework/exercises/1-list-of-names/script.js
@@ -0,0 +1,11 @@
+function listOfNames(arrayOfPeople) {
+  let content = document.querySelector("#content")
+}
+
+let people = [
+  { name: "Alireza", job: "Teacher" },
+  { name: "Jafar", job: "Student" },
+  { name: "Boris", job: "Prime Minister" },
+]
+
+listOfNames(people)
