4ef16cea05ce98525aa5738cd728a5f99895725e
Amir Pourhadi <Alex.ce1379@gmail.com>
2021-05-05 07:16:17 +0430

    quiz8 done

--- quiz8.js
+++ quiz8.js
@@ -5,3 +5,12 @@
 //1- create 100 new button elements
 //2- each button must have some text inside of it (it doesn't matter what)
 //3- each button must be appended inside the container div
+
+for (let i = 0; i < 100; i++) {
+	const newButton = document.createElement("button");
+	newButton.append("Button #" + (i + 1));
+	newButton.style.width = "100px";
+	newButton.style.padding = "5px";
+	newButton.style.margin = "5px";
+	document.getElementById("container").appendChild(newButton);
+}
